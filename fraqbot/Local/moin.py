155068c727364791a2926eca23140ea51611ecd0
root <drew@caffdev.com>
2020-10-16 14:29:11 +0000

    refactor moin to use its own rate config

--- fraqbot/Local/moin.py
+++ fraqbot/Local/moin.py
@@ -1,10 +1,15 @@
-import json
 import logging
+import os
+import sys
+import time
 
 from Legobot.Lego import Lego
-import requests
 
-from .helpers import call_rest_api
+LOCAL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+if LOCAL_DIR not in sys.path:
+    sys.path.append(LOCAL_DIR)
+
+from helpers import call_rest_api  # noqa #402
 
 
 logger = logging.getLogger(__name__)
@@ -12,33 +17,10 @@ logger = logging.getLogger(__name__)
 
 class Moin(Lego):
     def __init__(self, baseplate, lock, *args, **kwargs):
-        super().__init__(baseplate, lock, acl=kwargs.get('acl'),
-                         rate_config=kwargs.get('rate_config', {}))
+        super().__init__(baseplate, lock, acl=kwargs.get('acl'))
         self.url_base = kwargs.get('url_base')
         self.api_base = kwargs.get('api_base')
-        self._update_acl()
-
-    def _update_acl(self):
-        if not self.acl:
-            self.acl = {}
-
-        if 'whitelist' not in self.acl:
-            self.acl['whitelist'] = sorted([
-                k for k in
-                self._call_api('get', self.api_base).keys()
-            ])
-
-    def _call_api(self, method, url, payload=None):
-        if method == 'get':
-            response = requests.get(url)
-            if response.status_code == requests.codes.ok:
-                response = json.loads(response.text)
-            else:
-                response = None
-        else:
-            response = None
-
-        return response
+        self.rate_map = {}
 
     def _get_user_moin(self, user):
         url = f'{self.api_base}/{user}'
@@ -48,14 +30,31 @@ class Moin(Lego):
         else:
             return None
 
+    def _check_rate(self, source_user):
+        if not source_user:
+            return False
+
+        now = int(time.time())
+        last = self.rate_map.get(source_user, 0)
+        if now - last >= 300:
+            self.rate_map[source_user] = now
+            return True
+
+        return False
+
     def listening_for(self, message):
-        return 'moin' in str(message.get('text')).lower()
+        return 'moin' in str(message.get('text', '')).lower()
 
     def handle(self, message):
-        moin = self._get_user_moin(message['metadata']['source_user'])
-        if moin:
-            opts = self.build_reply_opts(message)
-            self.reply_attachment(message, 'moin', moin, opts=opts)
+        source_user = message.get('metadata', {}).get('source_user', '')
+        logger.debug(f'HANDLING MOIN for {source_user}')
+        check = self._check_rate(source_user)
+        logger.debug(f'CHECK RATE for {source_user}: {check}')
+        if check:
+            moin = self._get_user_moin(source_user)
+            if moin:
+                opts = self.build_reply_opts(message)
+                self.reply_attachment(message, 'moin', moin, opts=opts)
 
     def get_name(self):
         return ''
