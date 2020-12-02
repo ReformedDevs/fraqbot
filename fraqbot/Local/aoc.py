fa6df851a42c11d1159912d4c27b30b16ddb85c5
Drew Pearce <drew@caffdev.com>
2020-12-02 10:26:40 -0500

    update aoc lego

--- fraqbot/Local/aoc.py
+++ fraqbot/Local/aoc.py
@@ -1,8 +1,18 @@
 from decimal import Decimal
-import json
-from Legobot.Lego import Lego
 import logging
-import requests
+import os
+import sys
+
+import jmespath
+from Legobot.Connectors.Slack import Slack
+from Legobot.Lego import Lego
+
+LOCAL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+if LOCAL_DIR not in sys.path:
+    sys.path.append(LOCAL_DIR)
+
+from helpers import call_rest_api  # noqa 402
+from helpers import format_table  # noqa 402
 
 logger = logging.getLogger(__name__)
 TWOPLACES = Decimal(10) ** -2
@@ -11,149 +21,121 @@ TWOPLACES = Decimal(10) ** -2
 class AOC(Lego):
     def __init__(self, baseplate, lock, *args, **kwargs):
         super().__init__(baseplate, lock, acl=kwargs.get('acl'))
+        children = self.baseplate._actor.children
+        if children:
+            slack = [a._actor for a in children if isinstance(a._actor, Slack)]
+            if slack:
+                self.botThread = slack[0].botThread
+
         self.cookie = kwargs.get('cookie')
         self.year = kwargs.get('year')
         self.board = kwargs.get('board')
+        self.user_map = kwargs.get('user_map', {})
 
     def listening_for(self, message):
-        if message.get('text') and self.cookie:
-            try:
-                return message.get('text').lower().startswith('!aoc')
-            except Exception as e:
-                logger.error(('AOC lego failed to check the message text:'
-                             ' {}').format(e))
-                return False
+        text = message.get('text', '')
+        if not isinstance(text, str) or not self.cookie:
+            return False
+
+        return text.lower().startswith('!aoc')
 
     def handle(self, message):
-        opts = self.build_reply_opts(message)
         response = self._build_response(message['text'])
         if response:
+            opts = self.build_reply_opts(message)
             self.reply(message, response, opts)
 
     def _get_leaderboard(self):
         headers = {'Cookie': self.cookie}
         url = (f'https://adventofcode.com/{self.year}/leaderboard/private/view'
                f'/{self.board}.json')
-        get_leaderboard = requests.get(url, headers=headers)
-        if get_leaderboard.status_code == requests.codes.ok:
-            return json.loads(get_leaderboard.text)
-        else:
-            logger.warning(
-                'GET LEADERBOARD FAILED. CODE: {}. MESSAGE {}.'.format(
-                    get_leaderboard.status_code,
-                    get_leaderboard.text
-                )
-            )
-            return None
+        return call_rest_api(
+            __name__, 'get', url, response='json', headers=headers)
+
+    def _sort_leaderboard(self, leaderboard, params):
+        sort_key = 'score'
+        reverse = True
+
+        if len(params) > 1:
+            sort_key = params[1] if params[1] in leaderboard[0] else sort_key
+
+        if len(params) > 2:
+            reverse = False if params[2].lower() == 'asc' else True
+
+        srch = ('[].{name: slack_name, score: local_score, '
+                'stars: stars, pika: pika}')
+        leaders = sorted(
+            jmespath.search(srch, leaderboard),
+            key=lambda k: k[sort_key],
+            reverse=reverse
+        )
+
+        return leaders
+
+    def _get_slack_name(self, _id, name):
+        if _id in self.user_map:
+            name = self.botThread.get_user_name_by_id(
+                self.user_map[_id], True, name)
+
+        return name
 
     def _build_response(self, message):
-        sort_key_map = {
-            'score': 'score',
-            'stars': 'stars',
-            'name': 'name',
-            'crank': 'crank',
-            'chuck': 'crank'
-        }
-        reverse_map = {
-            'asc': False,
-            'desc': True
-        }
-        if len(message.split(' ')) > 2:
-            reverse = message.split(' ')[2].lower()
-        else:
-            reverse = 'desc'
-
-        if len(message.split(' ')) > 1:
-            sort_key = message.split(' ')[1].lower()
-            if sort_key in reverse_map.keys():
-                reverse = sort_key
-                sort_key = 'score'
-        else:
-            sort_key = 'score'
-
-        reverse = reverse_map.get(reverse, True)
-        sort_key = sort_key_map.get(sort_key, 'local_score')
         leaderboard = self._get_leaderboard()
         logger.debug('LEADERBOARD: {}'.format(leaderboard))
         if leaderboard:
-            leaderboard = {
-                k: v for k, v in leaderboard.get('members', {}).items()
-                if v.get('stars', 0) > 0
-            }
-            # leaderboard.get('members')
-            leaderboard = self._calculate_suplemental_values(leaderboard)
-            leaders = sorted(
-                        [{'name': value['name'],
-                          'score': value['local_score'],
-                          'stars': value['stars'],
-                          'crank': value['crank']}
-                         for k, value in leaderboard.items()],
-                        key=lambda k: k[sort_key],
-                        reverse=reverse)
-            leaders.insert(0, {
-                'name': 'NAME',
-                'score': 'SCORE',
-                'stars': 'STARS',
-                'crank': 'CHUCK INDEX'
-            })
-            max_len = {
-                'name': max([len(str(x['name'])) for x in leaders]) + 3,
-                'score': max([len(str(x['score'])) for x in leaders]) + 3,
-                'stars': max([len(str(x['stars'])) for x in leaders]) + 3,
-                'crank': max([len(str(x['crank'])) for x in leaders]) + 3
-            }
-            display_order = [
-                'name',
-                'score',
-                'stars',
-                'crank'
+            new = []
+            for k, v in leaderboard.get('members', {}).items():
+                if v.get('stars', 0) > 0:
+                    v.update({
+                        'id': k,
+                        'slack_name': self._get_slack_name(v['id'], v['name'])
+                    })
+                    new.append(v)
+
+            leaderboard = self._calculate_suplemental_values(new)
+            leaders = self._sort_leaderboard(leaderboard, message.split(' '))
+
+            fields = [
+                {'field': 'name', 'display': 'NAME'},
+                {'field': 'score', 'display': 'SCORE'},
+                {'field': 'stars', 'display': 'STARS'},
+                {'field': 'pika', 'display': 'PIKADEX'}
             ]
-            if sort_key != 'name':
-                display_order.remove(sort_key)
-                display_order.insert(1, sort_key)
+            leaders = format_table(leaders, fields=fields, sep='   ')
 
-            table = ''
-            for x in leaders:
-                x = {k: str(v) for k, v in x.items()}
-                for cat in display_order:
-                    while len(x[cat]) < max_len[cat]:
-                        x[cat] += ' '
-                    table += x[cat]
-                table += '\n'
-
-            response = '```{}```'.format(table)
-            return response
+            return f'```{leaders}```'
         else:
             return None
 
     def _calculate_suplemental_values(self, leaderboard):
-        chuck = [
+        pika = [
             {'stars': x['stars'], 'score': x['local_score']}
-            for k, x in leaderboard.items()
-            if x['name'] == 'sircharleswatson'
+            for x in leaderboard
+            if x['name'] == 'Patrick Carver'
         ]
-        chuck = chuck[0] if chuck else {'score': 0, 'stars': 0}
-        for k, v in leaderboard.items():
-            v['crank'] = self._calculate_chuck_rank(
-                v['local_score'],
-                chuck['score'],
-                v['stars'],
-                chuck['stars'],
-                max(v['stars'] for k, v in leaderboard.items())
+        pika = pika[0] if pika else{'score': 0, 'stars': 0}
+        max_stars = max(x['stars'] for x in leaderboard)
+        for item in leaderboard:
+            item['pika'] = self._calculate_pikadex(
+                item['local_score'],
+                pika['score'],
+                item['stars'],
+                pika['stars'],
+                max_stars
             )
 
         return leaderboard
 
-    def _calculate_chuck_rank(self, score, chuck_score, stars, chuck_stars,
-                              max_stars):
-        star_mod = stars - chuck_stars
-        score_mod = score - chuck_score
+    def _calculate_pikadex(self, score, p_score, stars, p_stars, max_stars):
+        star_mod = stars - p_stars
+        score_mod = score - p_score
         return Decimal((star_mod * score_mod) / max_stars).quantize(TWOPLACES)
 
     def get_name(self):
         return 'AOC'
 
     def get_help(self):
-        return ('Returns Advent of Code private leaderboard info.\n Usage !aoc'
-                ' [sort column] [sort direction]\nColumns: name, score, stars,'
-                ' chuck\nDirections: asc, desc')
+        return ('Returns Advent of Code private leaderboard info.\n'
+                'Usage !aoc [sort column] [sort direction]\n'
+                'Columns: name, score, stars, pika\n'
+                'Directions: asc, desc')
