b8648aae46e92d7968825443110b64e39a220629
Drew Pearce <drew@caffdev.com>
2021-08-05 11:17:54 -0400

    Update moin tests to reflect changes

--- fraqbot/tests/test_moin.py
+++ fraqbot/tests/test_moin.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import threading
-import time
 
 from Legobot.Lego import Lego
 from mock import patch
@@ -12,13 +11,24 @@ LOCAL_PATH = os.path.join(
     'Local'
 )
 sys.path.append(LOCAL_PATH)
+
 from moin import Moin  # noqa: E402
+
 
 URL_BASE = 'http://localhost/files/'
 API_BASE = 'http://localhost/api/moins'
 LOCK = threading.Lock()
 BASEPLATE = Lego.start(None, LOCK)
-LEGO = Moin(BASEPLATE, LOCK, url_base=URL_BASE, api_base=API_BASE)
+LEGO = Moin(
+    BASEPLATE,
+    LOCK,
+    url_base=URL_BASE,
+    api_base=API_BASE,
+    rate_config={
+        'rate_key': 'metadata.source_user',
+        'rate_interval': 300
+    }
+)
 
 
 def test_listening_for():
@@ -29,27 +39,6 @@ def test_listening_for():
     assert LEGO.listening_for({'text': 'MOIN'}) is True
 
 
-def test_check_rate():
-    now = int(time.time())
-    check = LEGO._check_rate('bob')
-    assert check is True
-    assert 'bob' in LEGO.rate_map
-    assert LEGO.rate_map['bob'] - now < 10
-
-    check = LEGO._check_rate('bob')
-    assert check is False
-
-    check = LEGO._check_rate('sally')
-    assert check is True
-
-    now = int(time.time())
-    LEGO.rate_map['bob'] = now - 300
-    assert check is True
-
-    assert 'bob' in LEGO.rate_map
-    assert 'sally' in LEGO.rate_map
-
-
 @patch('Legobot.Lego.Lego.reply')
 @patch('moin.Moin._get_user_moin')
 def test_handle_check(mock_get_user_moin, mock_reply):
@@ -57,8 +46,8 @@ def test_handle_check(mock_get_user_moin, mock_reply):
     mock_get_user_moin.return_value = 'http://localhost/files/bob.jpg'
     mock_reply.return_value = True
 
-    LEGO.handle(msg)
-    LEGO.handle(msg)
+    LEGO.on_receive(msg)
+    LEGO.on_receive(msg)
     mock_get_user_moin.assert_called_once()
 
 
