97619a126147f05ebd3fa429d03b7ea6b4c3aaf9
Ian Halverson <ihalverson@burrislogistics.com>
2021-09-22 11:49:27 -0400

    random emoji lego for fraqbot

---
+++ fraqbot/Local/randomemoji.py
@@ -0,0 +1,62 @@
+import logging
+import os
+import random
+import sys
+
+from Legobot.Lego import Lego
+
+
+logger = logging.getLogger(__name__)
+LOCAL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+
+if LOCAL_DIR not in sys.path:
+    sys.path.append(LOCAL_DIR)
+
+
+from helpers import utils  # noqa: E402
+
+
+class RandomEmoji(Lego):
+    def __init__(self, baseplate, lock, *args, **kwargs):
+        super().__init__(baseplate, lock, acl=kwargs.get('acl'))
+
+    def listening_for(self, message):
+        text = message.get('text')
+
+        return isinstance(text, str) and text.startswith('!emoji')
+
+    def _get_emoji(self, how_many):
+        maximum_how_many = 20
+        how_many_limited = min(maximum_how_many, how_many)
+        
+        emoji_response = utils.call_slack_api(
+                self.slack_client,
+                'emoji.list',
+                True,
+                'emoji',
+                total_limit=10000,
+                limit=1000,
+            )
+        
+        final_emoji_text = ""
+
+        x = range(int(how_many_limited))
+        for n in x:
+            final_emoji_text += ':' + random.choice(list(emoji_response.items()))[0] + ':'
+
+        return final_emoji_text
+
+    def handle(self, message):
+        logger.debug('Handling Random Emoji request: {}'.format(message['text']))
+        default_how_many = 5
+        how_many = int(message['text'][6:].strip() or default_how_many)
+
+        random_emojis = self._get_emoji(how_many)
+        opts = self.build_reply_opts(message)
+        self.reply(message, random_emojis, opts)
+
+    def get_name(self):
+        return 'Random_Emoji'
+
+    def get_help(self):
+        return 'Get a random Emoji (or multiple!). Usage: !emoji <how_many[default=5]>'
---
+++ fraqbot/tests/test_randomemoji.py
@@ -0,0 +1,52 @@
+import os
+import re
+import sys
+import threading
+
+from Legobot.Lego import Lego
+from mock import patch
+
+
+LOCAL_PATH = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    '..',
+    'Local'
+)
+sys.path.append(LOCAL_PATH)
+
+
+from randomemoji import RandomEmoji  # noqa: E402
+
+
+LOCK = threading.Lock()
+BASEPLATE = Lego.start(None, LOCK)
+LEGO = RandomEmoji(BASEPLATE, LOCK)
+
+
+def test_listening_for():
+    # !emoji
+    assert LEGO.listening_for({'text': 'emoji'}) is False
+    assert LEGO.listening_for({'text': '!emoji moin'}) is True
+    assert LEGO.listening_for({'text': '!emoji 12'}) is True
+    assert LEGO.listening_for({'text': '!Emoji'}) is False
+
+
+def test_get_emoji():
+    assert isinstance(LEGO._get_emoji(5), str)
+    assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(1))) == 1
+    assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(5))) == 5
+    assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(20))) == 20
+    assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(5000))) == 20
+
+
+@patch('Legobot.Lego.Lego.reply')
+@patch('randomemoji.RandomEmoji._get_emoji')
+def test_handle_check(mock_get_emoji, mock_reply):
+    msg = {'text': '!emoji 7', 'metadata': {'source_user': 'harold'}}
+    LEGO.handle(msg)
+
+    mock_get_emoji.assert_called_once()
+    mock_reply.assert_called_once()
+
+
+BASEPLATE.stop()
