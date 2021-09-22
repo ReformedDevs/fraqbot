2f94d0bc37e44a4023bf4560ecd85cdc04041adb
Ian Halverson <halversoncode@gmail.com>
2021-09-22 16:26:49 -0400

    Feature / Random Emoji (#49)
    * random emoji lego for fraqbot
    
    * add minimum of 1
    
    * add space between each emoji
    
    * adjustments from PR review
    
    * syntax fix
    
    * linting fix
    
    * get slack_client and set to RandomEmojis.client
    
    * import Slack from connectors
    
    * convert vars to class properties
    
    * improve tests and fix errors when invalid how_many
    
    * syntax
    
    Co-authored-by: Ian Halverson <ihalverson@burrislogistics.com>

---
+++ fraqbot/Local/randomemoji.py
@@ -0,0 +1,88 @@
+import logging
+import os
+import random
+import sys
+
+from Legobot.Connectors.Slack import Slack
+from Legobot.Lego import Lego
+
+
+logger = logging.getLogger(__name__)
+LOCAL_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+
+if LOCAL_DIR not in sys.path:
+    sys.path.append(LOCAL_DIR)
+
+
+from helpers import utils  # noqa: E402
+
+
+class RandomEmoji(Lego):
+    def __init__(self, baseplate, lock, *args, **kwargs):
+        super().__init__(baseplate, lock, acl=kwargs.get('acl'))
+        self._set_client()
+        self.max_how_many = 20
+        self.min_how_many = 1
+        self.default_how_many = 5
+
+    def listening_for(self, message):
+        text = message.get('text')
+
+        return isinstance(text, str) and text.startswith('!emoji')
+
+    def _set_client(self):
+        self.client = None
+        children = self.baseplate._actor.children
+
+        if children:
+            slack = [a._actor for a in children if isinstance(a._actor, Slack)]
+            if slack:
+                self.client = slack[0].botThread.slack_client
+
+    def _fetch_slack_emojis(self):
+        return utils.call_slack_api(
+                self.client,
+                'emoji.list',
+                False,
+                'emoji'
+            )
+
+    def _get_emoji(self, how_many):
+        how_many_limited = max(
+            self.min_how_many,
+            min(self.max_how_many, how_many)
+        )
+
+        emoji_response = self._fetch_slack_emojis()
+
+        return (':'
+                + ': :'.join(random.choices(
+                        list(emoji_response.keys()), k=how_many_limited
+                    )).strip()
+                + ':')
+
+    def handle(self, message):
+        logger.debug(
+            'Handling Random Emoji request: {}'.format(message['text'])
+        )
+        text_provided = message['text'][6:].strip()
+
+        opts = self.build_reply_opts(message)
+        if len(text_provided) > 0 and not text_provided.isdigit():
+            return self.reply(
+                message,
+                '\'{}\' is not a valid integer.'.format(text_provided),
+                opts
+            )
+
+        how_many = int(text_provided or self.default_how_many)
+
+        random_emojis = self._get_emoji(how_many)
+        self.reply(message, random_emojis, opts)
+
+    def get_name(self):
+        return 'Random_Emoji'
+
+    def get_help(self):
+        return ('Get a random Emoji (or multiple!).'
+                + 'Usage: !emoji <how_many[default=5]>')
---
+++ fraqbot/tests/test_randomemoji.py
@@ -0,0 +1,96 @@
+import os
+import pytest
+import re
+import sys
+import threading
+
+from Legobot.Lego import Lego
+from mock import patch
+
+
+LOCAL_PATH = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    '..',
+    'Local'
+)
+sys.path.append(LOCAL_PATH)
+
+
+from randomemoji import RandomEmoji  # noqa: E402
+
+
+LOCK = threading.Lock()
+BASEPLATE = Lego.start(None, LOCK)
+LEGO = RandomEmoji(BASEPLATE, LOCK)
+
+
+def test_listening_for():
+    # !emoji
+    assert LEGO.listening_for({'text': 'emoji'}) is False
+    assert LEGO.listening_for({'text': '!emoji moin'}) is True
+    assert LEGO.listening_for({'text': '!emoji 12'}) is True
+    assert LEGO.listening_for({'text': '!Emoji'}) is False
+
+
+def test_get_emoji():
+    with patch('randomemoji.RandomEmoji._fetch_slack_emojis') as mocked_fse:
+        mocked_fse.return_value = {'_man-shrugging': 'some_url'}
+        assert isinstance(LEGO._get_emoji(5), str)
+        assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(1))) == 1
+        assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(5))) == 5
+        assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(20))) == 20
+        assert len(re.findall(':[a-z0-9-_]+:', LEGO._get_emoji(5000))) == 20
+
+        assert LEGO._get_emoji(1) == ':_man-shrugging:'
+
+        five_long = (':_man-shrugging: :_man-shrugging:'
+                     + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:')
+        assert LEGO._get_emoji(5) == five_long
+
+        twenty_long = (':_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:'
+                       + ' :_man-shrugging: :_man-shrugging: :_man-shrugging:')
+        assert LEGO._get_emoji(20) == twenty_long
+        assert LEGO._get_emoji(5000) == twenty_long
+
+
+# !emoji 7
+@patch('Legobot.Lego.Lego.reply')
+@patch('randomemoji.RandomEmoji._get_emoji')
+def test_handle_check(mock_get_emoji, mock_reply):
+    msg = {'text': '!emoji 7', 'metadata': {'source_user': 'harold'}}
+    LEGO.handle(msg)
+
+    mock_get_emoji.assert_called_once()
+    mock_reply.assert_called_once()
+
+
+# !emoji
+@patch('Legobot.Lego.Lego.reply')
+@patch('randomemoji.RandomEmoji._get_emoji')
+def test_handle_check_2(mock_get_emoji, mock_reply):
+    msg = {'text': '!emoji', 'metadata': {'source_user': 'harold'}}
+    LEGO.handle(msg)
+
+    mock_get_emoji.assert_called_once()
+    mock_reply.assert_called_once()
+
+
+# !emoji bob
+@patch('Legobot.Lego.Lego.reply')
+@patch('randomemoji.RandomEmoji._get_emoji')
+def test_handle_check_3(mock_get_emoji, mock_reply):
+    msg = {'text': '!emoji bob', 'metadata': {'source_user': 'harold'}}
+    LEGO.handle(msg)
+
+    # Make sure get_emoji was not called
+    with pytest.raises(AssertionError):
+        mock_get_emoji.assert_called_once()
+    mock_reply.assert_called_once()
+
+
+BASEPLATE.stop()
